1c95339cecbcb23ebd5c7a2865c4e0e486b663d3
Haymanot W <haymanotalemu21@gmail.com>
2020-08-21 16:16:05 -0400

    client side code

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
-INSERT INTO burgers (burger_name) VALUES ('Checken Bacon Burger');
-INSERT INTO burgers (burger_name) VALUES ('Beef Double Cheese Burger');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Luther Burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Double Patty Burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('The Lola Burger
-', true);
-INSERT INTO burgers (burger_name) VALUES ('Dyer’s Deep-Fried Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Checken Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beef Double Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegi Burger', );
+-- INSERT INTO burgers (burger_name, devoured) VALUES ('Double Patty Burger', true);
+-- INSERT INTO burgers (burger_name, devoured) VALUES ('The Lola Burger
+-- ', true);
+-- INSERT INTO burgers (burger_name) VALUES ('Dyer’s Deep-Fried Burger');
