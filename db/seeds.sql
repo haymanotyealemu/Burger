9af25b397882c78e940c5c9620dc03c00a06ff98
Haymanot W <haymanotalemu21@gmail.com>
2020-08-23 16:45:13 -0400

    changed connection.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,3 @@
 INSERT INTO burgers (burger_name) VALUES ('Checken Bacon Burger');
 INSERT INTO burgers (burger_name) VALUES ('Beef Double Cheese Burger');
 INSERT INTO burgers (burger_name) VALUES ('Vegi Burger');
--- INSERT INTO burgers (burger_name, devoured) VALUES ('Double Patty Burger', true);
--- INSERT INTO burgers (burger_name, devoured) VALUES ('The Lola Burger
--- ', true);
--- INSERT INTO burgers (burger_name) VALUES ('Dyer’s Deep-Fried Burger');
